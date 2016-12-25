f46e94a257331f4cb189ff271b8618975892f75e
nikowis <nikowis@gmail.com>
2016-12-25 22:46:30 +0100

    Optimized rendering of objects that are behind the camera.

--- src/pl/nikowis/renderEngine/Renderer.java
+++ src/pl/nikowis/renderEngine/Renderer.java
@@ -25,6 +25,8 @@ public class Renderer {
     private Matrix4f projectionMatrix;
 
     public Renderer(StaticShader shader) {
+        GL11.glEnable(GL11.GL_CULL_FACE);
+        GL11.glCullFace(GL11.GL_BACK);
         createProjectionMatrix();
         shader.start();
         shader.loadProjectionMatrix(projectionMatrix);
