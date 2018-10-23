0fe95c3abae8b3547ee12db6d99fcbf5ba7ce905
nikowis <nikowis@gmail.com>
2018-10-23 23:22:26 +0200

    Temporary commit - camera movement.

---
+++ src/pl/nikowis/CameraTester.java
@@ -0,0 +1,95 @@
+package pl.nikowis;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
+import pl.nikowis.entities.CameraManager;
+import pl.nikowis.entities.Entity;
+import pl.nikowis.entities.Light;
+import pl.nikowis.entities.MovingCamera;
+import pl.nikowis.entities.MovingEntity;
+import pl.nikowis.entities.StaticCamera;
+import pl.nikowis.entities.ThirdPersonCamera;
+import pl.nikowis.entities.TurningCamera;
+import pl.nikowis.models.FullModel;
+import pl.nikowis.renderEngine.DisplayManager;
+import pl.nikowis.renderEngine.Loader;
+import pl.nikowis.renderEngine.MasterRenderer;
+import pl.nikowis.renderEngine.OBJLoader;
+import pl.nikowis.terrains.Terrain;
+import pl.nikowis.textures.ModelTexture;
+import pl.nikowis.textures.TerrainTexture;
+import pl.nikowis.textures.TerrainTexturePack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Main class.
+ * Created by Nikodem on 12/22/2016.
+ */
+public class CameraTester {
+
+    private static float atBaseFact = 1;
+    private static float atDistFact = 0f;
+    private static float atDistSquareFact = 0.001f;
+    private static Vector3f atenuation = new Vector3f(atBaseFact, atDistFact, atDistSquareFact);
+
+    public static void main(String[] args) {
+
+        DisplayManager.createDisplay();
+        Loader loader = new Loader();
+
+        //###############################   MODELS  ##########################
+        FullModel staticBoxModel = new FullModel(OBJLoader.loadObjModel("box", loader), new ModelTexture(loader.loadTexture("box")), new Vector3f(0.62f, 0.32f, 0.176f));
+        FullModel staticSphereModel = new FullModel(OBJLoader.loadObjModel("sphere", loader), null, new Vector3f(0.82f, 0.82f, 0.82f));
+        //####################################################################
+
+        //###############################   ENTITIES  ########################
+        List<Entity> entities = new ArrayList<>();
+        MovingEntity boxEntity = new MovingEntity(staticBoxModel, new Vector3f(696, 3, 183), 0, 33, 0, 3);
+        entities.add(boxEntity);
+        entities.add(new Entity(staticSphereModel, new Vector3f(696, 3, 183), 0, 33, 0, 3));
+        List<Light> lights = new ArrayList<>();
+        Light sunLight = new Light(new Vector3f(800, 10000, 800), new Vector3f(0.4f, 0.4f, 0.4f));
+        lights.add(sunLight);
+        //####################################################################
+
+        //###############################   TERRAIN  #########################
+        TerrainTexture backgroundTexture = new TerrainTexture(loader.loadTexture("grass"));
+        TerrainTexture rTexture = new TerrainTexture(loader.loadTexture("mud"));
+        TerrainTexture gTexture = new TerrainTexture(loader.loadTexture("grassFlowers"));
+        TerrainTexturePack texturePack = new TerrainTexturePack(backgroundTexture, rTexture, gTexture, gTexture);
+        TerrainTexture blendMap = new TerrainTexture(loader.loadTexture("blendMap"));
+        Terrain terrain = new Terrain(0, 0, loader, texturePack, blendMap, new Vector3f(0.3f, 0.3f, 0.3f));
+        //####################################################################
+
+        //###############################   CAMERAS  #########################
+        StaticCamera staticCamera = new StaticCamera(new Vector3f(0, 50, 0), 10);
+        staticCamera.setYaw(130);
+        TurningCamera turningCamera = new TurningCamera(new Vector3f(0, 50, 0), 10, boxEntity);
+        MovingCamera movingCamera = new MovingCamera(new Vector3f(0, 150, 0), new Vector3f(0, 0, 0));
+        movingCamera.setPitch(10);
+        movingCamera.setRoll(110);
+        CameraManager cameraManager = new CameraManager(staticCamera, movingCamera, turningCamera);
+        //####################################################################
+
+        MasterRenderer masterRenderer = new MasterRenderer();
+        masterRenderer.processTerrain(terrain);
+        for (Entity entity : entities) {
+            masterRenderer.processEntity(entity);
+        }
+
+        while (!Display.isCloseRequested()) {
+            cameraManager.checkInput();
+            cameraManager.moveCurrentCamera();
+            masterRenderer.checkInput();
+            masterRenderer.render(lights, cameraManager.getCurrentCamera());
+            DisplayManager.updateDisplay();
+        }
+
+        masterRenderer.cleanUp();
+        loader.cleanUp();
+        DisplayManager.closeDisplay();
+    }
+
+}
--- src/pl/nikowis/entities/MovingCamera.java
+++ src/pl/nikowis/entities/MovingCamera.java
@@ -1,21 +1,59 @@
 package pl.nikowis.entities;
 
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.util.vector.Vector3f;
+import pl.nikowis.renderEngine.DisplayManager;
 
 /**
  * Camera that moves.
  * Created by Nikodem on 12/25/2016.
  */
-public class MovingCamera extends ThirdPersonCamera {
+public class MovingCamera extends Camera {
 
-    public MovingCamera(Vector3f followPosition, Vector3f initialRotation) {
-        super(new MovingEntity(null, followPosition, initialRotation.x, initialRotation.y, initialRotation.z, 0));
+    protected float move_speed = 200;
+    protected float turn_speed = 200;
+
+    protected float currentSpeed = 0;
+    protected float currentTurnSpeed = 0;
+
+    public MovingCamera(Vector3f position) {
+        this.position = position;
         pitch = 20;
     }
 
     @Override
     public void move() {
-        ((MovingEntity) entity).move();
-        super.move();
+        checkInputs();
+        performMove();
+    }
+
+    protected void performMove() {
+        float adjustedTurnSpeed = currentTurnSpeed * DisplayManager.getFrameTimeSeconds();
+
+
+
+        float distance = currentSpeed * DisplayManager.getFrameTimeSeconds();
+        float dx = (float) (distance * Math.sin(Math.toRadians(super.getRotY())));
+        float dz = (float) (distance * Math.cos(Math.toRadians(super.getRotY())));
+        super.increasePosition(dx, 0, dz);
+    }
+
+    protected void checkInputs() {
+        if (Keyboard.isKeyDown(Keyboard.KEY_UP)) {
+            currentSpeed = +move_speed;
+        } else if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
+            currentSpeed = -move_speed;
+        } else {
+            this.currentSpeed = 0;
+        }
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
+            this.currentTurnSpeed = -turn_speed;
+        } else if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
+            this.currentTurnSpeed = turn_speed;
+        } else {
+            this.currentTurnSpeed = 0;
+        }
+
     }
 }
